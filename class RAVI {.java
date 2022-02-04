eac14ea8113d47943afaa3eb0f7f1f21a9015e6a
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2022-02-04 22:38:59 +0530

    Add files via upload

---
+++ class RAVI {.java
@@ -0,0 +1,10 @@
+class Ravi{
+    public static void main(String args[]){
+        double x;
+        x = 7.8;
+        System.out.println("hello world");
+        System.out.print(" your result is" + x);
+    }
+
+}
+
