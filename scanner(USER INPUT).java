bfd38f110367663ddc72103c5d57b639499c9e84
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2022-02-06 19:19:03 +0530

    Add files via upload

---
+++ scanner(USER INPUT).java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class scanner{
+    public static void main(String args[]){
+        Scanner tcw = new Scanner (System.in);
+        System.out.println(tcw.nextLine());
+    }
+}
+
+
+    
+
